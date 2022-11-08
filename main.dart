61fc230f8e771e1f52b1d863740436b2a5ad28c4
amaghrabi7 <abdul.maghrabi@gmail.com>
2022-11-08 19:58:37 +0300

    finished the task

--- main.dart
+++ main.dart
@@ -1,7 +1,28 @@
-void main(){
-  // Write your code here
-  
-  
-  
-  
+void main() {
+  List<int> numbers = [5, 3, 15, 4, 1];
+
+  int sum = 0;
+  numbers.forEach((int number) => sum += number);
+  print(sum);
+
+  int max = 0;
+  numbers.forEach((int number) {
+    if (number > max) {
+      max = number;
+    }
+  });
+  print(max);
+
+  List<int> filterVisitors(List<int> ages, int minAge) {
+    return ages.where((int age) => age > minAge).toList();
+  }
+
+  print(filterVisitors([20, 3, 40, 6, 13, 26], 18));
+
+  int findOdds(List<int> numbers) {
+    return numbers.firstWhere((int number) => (number % 2) != 0,
+        orElse: () => 0);
+  }
+
+  print(findOdds([10, 4, 6, 12, 22, 2]));
 }
